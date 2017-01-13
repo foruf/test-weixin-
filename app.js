2851444d34038c495205db45802b4b1b64cf627f
foruf <foru@sina.com>
2017-01-13 10:44:34 +0800

    发布

---
+++ app.js
@@ -0,0 +1,44 @@
+//app.js
+App({
+    onLaunch: function () {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+    },
+    getUserInfo: function (cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function (result) {
+
+                    /*const code = result.code;
+                    console.log(code);
+
+                    wx.request({
+                        url: 'https://www.chinaclick.com.cn/reg_s.php',
+                        data: {
+                            code: result.code
+                        },
+                        success: function (result) {
+                            console.log(result);
+                        }
+                    })*/
+
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
+        }
+    },
+    globalData: {
+        userInfo: null
+    }
+})
